af29f07691c42c723f1f89240f5a8625298ac05c
zheniatv <zheniatv@gmail.com>
2020-09-10 23:12:21 +0300

    KrestikiNoliki

--- app/src/main/java/com/example/krestikinoliki/MainActivity.java
+++ app/src/main/java/com/example/krestikinoliki/MainActivity.java
@@ -3,12 +3,136 @@ package com.example.krestikinoliki;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Random;
 
+public class MainActivity extends AppCompatActivity
+{
+    TextView textView;
+    TextView textView6;
+    TextView textView7;
+    Button button1;
+    Button button2;
+    Button button3;
+    Button button4;
+    Button button5;
+    Button button6;
+    Button button7;
+    Button button8;
+    Button button9;
+    Button buttonStart;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView) findViewById(R.id.textView);
+        textView6 = (TextView) findViewById(R.id.textView6);
+        textView7 = (TextView) findViewById(R.id.textView7);
+
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+        button5 = (Button) findViewById(R.id.button5);
+        button6 = (Button) findViewById(R.id.button6);
+        button7 = (Button) findViewById(R.id.button7);
+        button8 = (Button) findViewById(R.id.button8);
+        button9 = (Button) findViewById(R.id.button9);
+        buttonStart = (Button) findViewById(R.id.buttonStart);
+
+        buttonStart.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                Random random = new Random();
+                int rnd = random.nextInt(10);
+                textView.setText("" + (rnd + 1));
+                if (rnd >= 5) {
+                    textView6.setText("YES");
+                }
+                else {
+                textView7.setText("NO");
+                }
+
+            }
+        });
+        button1.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+            button1.setText("X");
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                button2.setText("X");
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                button3.setText("X");
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                button4.setText("X");
+            }
+        });
+        button5.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                button5.setText("X");
+            }
+        });
+        button6.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                button6.setText("X");
+            }
+        });
+        button7.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                button7.setText("X");
+            }
+        });
+        button8.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                button8.setText("X");
+            }
+        });
+        button9.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                button9.setText("X");
+            }
+        });
     }
 }
